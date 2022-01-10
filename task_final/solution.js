39b094e79bb5d48a5b9242de423054d0308e33a9
Lidana Bondar <lidanabondar@gmail.com>
2022-01-10 18:16:49 +0300

    Task_final done v1

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,18 +36,20 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < goods.length; i += 1) {
+        const goodObj = {
+            title: goods[i].title,
+            count: goods[i].count
+        };
+        data.goods.push(goodObj);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = `${name} ${phone}`;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
